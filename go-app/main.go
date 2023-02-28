d5142a1f359c0a46467e666f2be69c49bfba8c57
Alexey Borodulin <antifootbolist@gmail.com>
2023-02-28 15:11:41 +1000

    Remove comma from output of Go app

--- go-app/main.go
+++ go-app/main.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
     http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        fmt.Fprintf(w, "Hello, World from Go App\n")
+        fmt.Fprintf(w, "Hello World from Go App\n")
     })
 
     http.ListenAndServe(":8080", nil)
