afad8528b069f60c6bd131904dc6ae62c0fa3f71
Alexey Borodulin <antifootbolist@gmail.com>
2023-02-28 14:14:38 +1000

    Change output from apps

--- go-app/main.go
+++ go-app/main.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
     http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        fmt.Fprintf(w, "Hello, World from Go App")
+        fmt.Fprintf(w, "Hello, World from Go App\n")
     })
 
     http.ListenAndServe(":8080", nil)
