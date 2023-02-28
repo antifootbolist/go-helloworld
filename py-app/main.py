409c775f13c9a796baefa697371f241b898f278f
Alexey Borodulin <antifootbolist@gmail.com>
2023-02-28 17:56:45 +1000

    Fix port of upstreams

--- py-app/main.py
+++ py-app/main.py
@@ -2,9 +2,12 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
+
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
 def hello_world():
     return "Hello World from Python App\n"
+
 
 if __name__ == '__main__':
     app.run(port=8080, host='0.0.0.0')
